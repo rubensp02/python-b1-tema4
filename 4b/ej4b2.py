b1ad306be151c02293d27375a91f96295ada17d2
rubensp02 <rubensp02@gmail.com>
2026-06-17 18:03:32 +0200

    HECHO Y CORREGIDO

--- 4b/ej4b2.py
+++ 4b/ej4b2.py
@@ -50,23 +50,25 @@ Exemple:
 
 
 def average_score_ram(scores_dict):
-    # Store the dictionary in RAM
-    # Write here your code
-    pass
+    total = 0
 
+    for nota in scores_dict.values():
+        total += nota
+
+    return total / len(scores_dict)
 
 def average_score_heap(scores_dict):
     # Store the dictionary in Heap    
     score_list = list(scores_dict.values())
     # You should correct and overwrite something in the following line.
-    heap_average = sum(score_list) 
+    heap_average = (sum(score_list)) / (len(scores_dict.values())) 
     return heap_average
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# scores_dict = {"Juan": 6.7, "Maria": 9.1, "Pedro": 6.5, "Tomas": 8.2, "Julio": 9}
+scores_dict = {"Juan": 6.7, "Maria": 9.1, "Pedro": 6.5, "Tomas": 8.2, "Julio": 9}
 
-# print(average_score_ram(scores_dict))
-# print(average_score_heap(scores_dict))
+print(average_score_ram(scores_dict))
+print(average_score_heap(scores_dict))
